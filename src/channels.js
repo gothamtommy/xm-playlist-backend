e4372d175ec15c7c0453d20187d3235611f8132c
Scott Cooper <scttcper@gmail.com>
2016-10-26 13:10:47 -0700

    add number

--- src/channels.js
+++ src/channels.js
@@ -1,4 +1,13 @@
+// http://www.siriusxm.com/albumart/Live/Default/DefaultMDS_m_52.jpg
 module.exports = [
-  { name: 'BPM', id: 'thebeat' },
-  { name: 'Electric Area', id: 'area33' },
+  {
+    name: 'BPM',
+    id: 'thebeat',
+    channel: 51,
+  },
+  {
+    name: 'Electric Area',
+    id: 'area33',
+    channel: 52,
+  },
 ];

f8c421feb738d3d232372dcc1728b4bc000e1f06
Scott Cooper <scttcper@gmail.com>
2016-10-29 18:41:03 -0700

    share connection

--- src/mongo.js
+++ src/mongo.js
@@ -2,10 +2,15 @@ const MongoClient = require('mongodb');
 
 const config = require('../config');
 
-module.exports = MongoClient.connect(config.db, {
-  server: {
-    poolSize: 5,
-  },
-  w: 0,
-  auto_reconnect: true,
+module.exports = new Promise((resolve) => {
+  MongoClient.connect(config.db, {
+    server: {
+      poolSize: 5,
+    },
+    w: 0,
+    auto_reconnect: true,
+  }, (err, database) => {
+    if (err) throw err;
+    resolve(database);
+  });
 });

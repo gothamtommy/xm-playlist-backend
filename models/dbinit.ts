29fb6ed3a5404c14b3024a539ac29d12d045dcd1
Scott Cooper <scttcper@gmail.com>
2017-08-03 14:38:29 -0700

    sleep after db

--- models/dbinit.ts
+++ models/dbinit.ts
@@ -1,12 +1,17 @@
 import { Track, Artist, ArtistTrack, Play, Spotify } from './index';
 
+function sleep(time: number) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
 export function setup(force = false): any {
   const opt = { force };
   return Track.sync(opt)
     .then(() => Artist.sync(opt))
     .then(() => ArtistTrack.sync(opt))
     .then(() => Play.sync(opt))
-    .then(() => Spotify.sync(opt));
+    .then(() => Spotify.sync(opt))
+    .then(() => sleep(2000));
 }
 
 if (!module.parent) {

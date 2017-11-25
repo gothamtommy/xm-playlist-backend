d6d62f055bc6ba397ee639c538301bd9f7ebfb51
Scott Cooper <scttcper@gmail.com>
2017-11-25 00:53:04 -0800

    meh

--- commands/cleanDatabase.ts
+++ commands/cleanDatabase.ts
@@ -14,28 +14,6 @@ async function loop() {
   if (playDeleted > 0) {
     await Play.destroy(query1);
   }
-
-  const query2 = {
-    where: {
-      updatedAt: { [Op.lte]: old },
-    },
-  };
-  const trackDeleted = await Track.count(query2);
-  console.log('trackDeleted', trackDeleted);
-  if (trackDeleted > 0) {
-    await Track.destroy(query2);
-  }
-
-  const query3 = {
-    where: {
-      trackId: { [Op.eq]: null },
-    },
-  };
-  const artistTrackDeleted = await ArtistTrack.count(query3);
-  console.log('artistTrackDeleted', artistTrackDeleted);
-  if (artistTrackDeleted > 0) {
-    await ArtistTrack.destroy(query3);
-  }
 }
 
 loop()

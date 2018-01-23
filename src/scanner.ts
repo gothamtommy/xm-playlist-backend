3847603dca58f3c9525ea4343b5a827125835f5c
Scott Cooper <scttcper@gmail.com>
2018-01-22 21:56:36 -0800

    update dependencies, attempt to slim scanner memory

--- src/scanner.ts
+++ src/scanner.ts
@@ -7,24 +7,25 @@ import config from '../config';
 
 const log = debug('xmplaylist');
 
-const sentry = Raven.config(config.dsn, { autoBreadcrumbs: true });
+const sentry = Raven.config(config.dsn, { autoBreadcrumbs: false });
 if (config.dsn) {
   sentry.install({ captureUnhandledRejections: true });
 }
 
 export default function sleep(duration: number) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => { resolve(0); }, duration);
+    setTimeout(() => {
+      resolve(0);
+    }, duration);
   });
 }
 
 async function updateAll() {
-  while (true) {
-    for (const channel of channels) {
-      await checkEndpoint(channel).catch((e) => catchError(e));
-      await sleep(300);
-    }
+  for (const channel of channels) {
+    await checkEndpoint(channel).catch((e) => catchError(e));
+    await sleep(300);
   }
+  updateAll().catch((e) => catchError(e));
 }
 
 if (!module.parent) {
@@ -35,4 +36,5 @@ if (!module.parent) {
 function catchError(err: Error) {
   log(err);
   sentry.captureException(err);
+  process.exit(0);
 }

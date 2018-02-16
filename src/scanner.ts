9db5bb24861e9e06144f32f52c066ea739caa122
Scott Cooper <scttcper@gmail.com>
2018-02-15 23:05:16 -0800

    use pre built delay

--- src/scanner.ts
+++ src/scanner.ts
@@ -1,5 +1,7 @@
 import * as debug from 'debug';
 import * as Raven from 'raven';
+import * as delay from 'delay';
+import * as pForever from 'p-forever';
 
 import { checkEndpoint } from './sirius';
 import { channels } from './channels';
@@ -8,29 +10,18 @@ import config from '../config';
 const log = debug('xmplaylist');
 
 const sentry = Raven.config(config.dsn, { autoBreadcrumbs: false });
-if (config.dsn) {
-  sentry.install({ captureUnhandledRejections: true });
-}
-
-export default function sleep(duration: number) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(0);
-    }, duration);
-  });
-}
 
 async function updateAll() {
   for (const channel of channels) {
-    await checkEndpoint(channel).catch((e) => catchError(e));
-    await sleep(300);
+    await checkEndpoint(channel).catch((e: Error) => catchError(e));
+    await delay(300);
   }
-  updateAll().catch((e) => catchError(e));
+  return updateAll();
 }
 
 if (!module.parent) {
   log('cron running');
-  updateAll().catch((e) => catchError(e));
+  pForever(() => updateAll()).catch((e: Error) => catchError(e));
 }
 
 function catchError(err: Error) {
